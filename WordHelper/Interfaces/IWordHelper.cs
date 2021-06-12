afd2f51d76ae0188fb8e048ccf65e001f2587d8d
Mark Young <markyoung213@googlemail.com>
2021-06-12 13:09:19 +0100

    Added interface

---
+++ WordHelper/Interfaces/IWordHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace WordHelper.Interfaces
+{
+    public interface IWordHelper
+    {
+        public bool IsAnagram(string word1, string word2);
+        public bool IsPalindrome(string word);
+    }
+}
