29634228f620d6392729e7948269be86cccc65eb
Mark Young <markyoung213@googlemail.com>
2021-06-12 13:23:21 +0100

    Added base word helper and implementation

---
+++ WordHelper/Helpers/WordHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WordHelper.Interfaces;
+
+namespace WordHelper.Helpers
+{
+    public class WordHelper : IWordHelper
+    {
+        public bool IsAnagram(string word1, string word2)
+        {
+            word1 = PrepareString(word1);
+            word2 = PrepareString(word2);
+
+            return String.Concat(word1.OrderBy(x => x)) == String.Concat(word2.OrderBy(x => x));
+        }
+
+        public bool IsPalindrome(string word)
+        {
+            return PrepareString(word) == String.Concat(PrepareString(word).Reverse());
+        }
+
+        private string PrepareString(string word)
+        {
+            Regex regex = new Regex(@"[^a-zA-Z]");
+            return regex.Replace(word, "").ToLower();
+        }
+    }
+}
